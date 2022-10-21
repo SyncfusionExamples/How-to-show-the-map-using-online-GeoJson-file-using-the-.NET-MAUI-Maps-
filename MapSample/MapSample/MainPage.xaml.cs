d091cb7c7279bc98ea8a41b4da0d42bb68d8665d
Vignesh Ramesh <92786682+vigneshrameshsync@users.noreply.github.com>
2022-10-21 18:52:21 +0530

    Update MainPage.xaml.cs

--- MapSample/MapSample/MainPage.xaml.cs
+++ MapSample/MapSample/MainPage.xaml.cs
@@ -8,11 +8,10 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-        SfMaps map = new SfMaps();
-        MapShapeLayer layer = new MapShapeLayer();
-        layer.ShapesSource = MapSource.FromUri(new Uri("https://cdn.syncfusion.com/maps/map-data/world-map.json"));
-        map.Layer = layer;
-        this.Content = map;
+        	// SfMaps map = new SfMaps();
+        	// MapShapeLayer layer = new MapShapeLayer();
+        	// layer.ShapesSource = MapSource.FromUri(new Uri("https://cdn.syncfusion.com/maps/map-data/world-map.json"));
+        	// map.Layer = layer;
+        	// this.Content = map;
     }
 }
-
